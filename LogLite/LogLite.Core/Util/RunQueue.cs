5bf2b3d172ded98dbfc994f4ca77ec7d7ab36205
callumg2895 <callumgoodin@gmail.com>
2020-03-25 19:27:47 +0000

    RunQueue class now uses condition variables for synchronisation.

--- LogLite/LogLite.Core/Util/RunQueue.cs
+++ LogLite/LogLite.Core/Util/RunQueue.cs
@@ -7,11 +7,10 @@ namespace LogLite.Core.Util
 {
 	internal class RunQueue : IDisposable
 	{
-		private const int IdleTimeoutMilliseconds = 100;
-
 		private readonly Queue<Action> _actions;
 		private readonly Thread _thread;
 		private readonly CancellationTokenSource _cancellationTokenSource;
+		private readonly object _lock;
 
 		public RunQueue()
 		{
@@ -20,6 +19,7 @@ namespace LogLite.Core.Util
 			_actions = new Queue<Action>();
 			_thread = new Thread(threadStart);
 			_cancellationTokenSource = new CancellationTokenSource();
+			_lock = new object();
 
 			_thread.Start();
 		}
@@ -27,10 +27,29 @@ namespace LogLite.Core.Util
 		public void Enqueue(Action action)
 		{
 			_actions.Enqueue(action);
+
+			lock (_lock)
+			{
+				/*
+				 * We've just added a new item to the queue, but if it was previously empty the thread that dequeues actions
+				 * will be stuck waiting indefinitely. We should notify this thread that it's time to wake up and do work.
+				 */
+
+				Monitor.Pulse(_lock);
+			}
 		}
 
 		public void Dispose()
 		{
+			lock (_lock)
+			{
+				/*
+				 * We need to stop the thread, but if the run queue is currently waiting it needs to be notified.
+				 */
+
+				Monitor.Pulse(_lock);
+			}
+
 			_cancellationTokenSource.Cancel();
 			_thread.Join();
 		}
@@ -41,7 +60,17 @@ namespace LogLite.Core.Util
 			{
 				if (_actions.Count == 0)
 				{
-					Thread.Sleep(IdleTimeoutMilliseconds);
+					/*
+					 * At this point, the run queue is currently empty. We now aquire the lock, release it, and wait to be 
+					 * notified about the next time we aquire the lock. This allows us to wait without specifying a timeout 
+					 * interval.
+					 */
+
+					lock (_lock)
+					{
+						Monitor.Wait(_lock);
+					}
+
 					continue;
 				}
 
