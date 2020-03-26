9684c88289c6cb64d2182ea7d604177e07fbb5bd
callumg2895 <callumgoodin@gmail.com>
2020-03-26 22:10:08 +0000

    Updated RunQueue code to be more readable.

--- LogLite/LogLite.Core/Util/RunQueue.cs
+++ LogLite/LogLite.Core/Util/RunQueue.cs
@@ -28,64 +28,62 @@ namespace LogLite.Core.Util
 		{
 			_actions.Enqueue(action);
 
-			lock (_lock)
-			{
-				/*
-				 * We've just added a new item to the queue, but if it was previously empty the thread that dequeues actions
-				 * will be stuck waiting indefinitely. We should notify this thread that it's time to wake up and do work.
-				 */
+			/*
+			 * We've just added a new item to the queue, but if it was previously empty the thread that dequeues actions
+			 * will be stuck waiting indefinitely. We should notify this thread that it's time to wake up and do work.
+			 */
 
-				Monitor.Pulse(_lock);
-			}
+			NotifyQueue();
 		}
 
 		public void Dispose()
 		{
 			_cancellationTokenSource.Cancel();
 
+			// We need to stop the thread, but if the run queue is currently waiting it needs to be notified.
+
+			NotifyQueue();
+			_thread.Join();
+		}
+
+		private void NotifyQueue()
+		{
 			lock (_lock)
 			{
-				/*
-				 * We need to stop the thread, but if the run queue is currently waiting it needs to be notified.
-				 */
-
 				Monitor.Pulse(_lock);
 			}
-
-			_thread.Join();
 		}
 
 		private void ProcessActions()
 		{
-			while (!_cancellationTokenSource.Token.IsCancellationRequested || _actions.Count > 0)
+			// Always process actions at least once.
+
+			do
 			{
-				if (_actions.Count == 0)
-				{
-					/*
-					 * At this point, the run queue is currently empty. We now aquire the lock, release it, and wait to be 
-					 * notified about the next time we aquire the lock. This allows us to wait without specifying a timeout 
-					 * interval.
-					 */
+				ProcessNextAction();
+			}
+			while (!_cancellationTokenSource.Token.IsCancellationRequested || _actions.Count > 0);
+		}
 
-					lock (_lock)
-					{
-						Monitor.Wait(_lock);
-					}
-
-					continue;
-				}
-
-				Action? action = _actions.Peek();
-
-				if (action == null)
-				{
-					_actions.Dequeue();
-					continue;
-				}
-
-				action.Invoke();
-
+		private void ProcessNextAction()
+		{
+			if (_actions.Count > 0)
+			{
+				_actions.Peek()?.Invoke();
 				_actions.Dequeue();
+
+				return;
+			}
+
+			/*
+			 * At this point, the run queue is currently empty. We now aquire the lock, release it, and wait to be 
+			 * notified about the next time we aquire the lock. This allows us to wait without specifying a timeout 
+			 * interval.
+			 */
+
+			lock (_lock)
+			{
+				Monitor.Wait(_lock);
 			}
 		}
 	}
