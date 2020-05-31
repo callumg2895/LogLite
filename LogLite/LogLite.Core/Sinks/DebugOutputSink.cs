8be8e3c86f0b79a521671bbcc805c1256c8ae196
callumg2895 <callumgoodin@gmail.com>
2020-05-31 16:18:33 +0100

    Added a Debugger output sink.

---
+++ LogLite/LogLite.Core/Sinks/DebugOutputSink.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace LogLite.Core.Sinks
+{
+    public class DebugOutputSink : Sink
+    {
+        public DebugOutputSink()
+            : base(null)
+        {
+
+        }
+
+        public DebugOutputSink(LogLevel filter)
+            : base(filter)
+        {
+
+        }
+
+        protected override void Flush()
+        {
+            Thread.Sleep(FlushTimeoutMilliseconds);
+
+            while (true)
+            {
+                LogStatement? statement;
+
+                lock (_lock)
+                {
+                    if (!_logQueue.TryDequeue(out statement))
+                    {
+                        break;
+                    }
+                }
+
+                Debug.WriteLine(statement.ToString());
+            }
+        }
+    }
+}
